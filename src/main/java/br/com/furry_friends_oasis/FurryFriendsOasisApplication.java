b126eb75800fd613a380b890bbe010c55e43cf0f
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2024-08-14 22:28:01 -0300

    FEAT: First commit

---
+++ src/main/java/br/com/furry_friends_oasis/FurryFriendsOasisApplication.java
@@ -0,0 +1,13 @@
+package br.com.furry_friends_oasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryFriendsOasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurryFriendsOasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furry_friends_oasis/FurryFriendsOasisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furry_friends_oasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurryFriendsOasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
